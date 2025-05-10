f1d0c39f232d9a3aec41733e20ab0cfccaf9448d
AkshAy-K125 <akshay.raviraj@gmail.com>
2025-05-10 19:01:55 +0530

    full blown vite structure added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from './assets/aroha_logo.svg'
+const App = () => {
+  return (
+    <div>
+      <img src={logo} alt="Aroha svg logo" />
+    </div>
+  )
+}
+
+export default App
